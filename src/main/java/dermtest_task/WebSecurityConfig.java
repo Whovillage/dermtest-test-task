3dc06c0100fbb2f6f3d071a514d90b75c2b04ff4
Kevin Kesküla <kevinkeskula@Kevins-Air.lan>
2022-12-04 17:54:27 +0200

    Disable csrf

---
+++ src/main/java/dermtest_task/WebSecurityConfig.java
@@ -0,0 +1,16 @@
+package dermtest_task;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable());
+    }
+}
